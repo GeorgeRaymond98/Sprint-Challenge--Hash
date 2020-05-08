840b7afb80647567f1c30d5f4d9b859e1c8f6c40
George Raymond <georgeraymond98@gmail.com>
2020-05-08 13:17:58 -0400

    ex1 and ex2 are done.

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -7,8 +7,19 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     
-    """
-    YOUR CODE HERE
-    """
+    flight_libary = dict() # Make dict
+    
+    for ticket in tickets:
+        flight_libary[ticket.source] = ticket.destination
 
+    print(flight_libary)
+
+    route = []
+    curr_destination = flight_libary["NONE"]
+    print(curr_destination)
+
+    while curr_destination != "NONE": # As long as curr_destination is not NONE  
+        route.append(curr_destination) # adds curr_destination to route 
+        curr_destination = flight_libary[curr_destination]
+    route.append("NONE")
     return route
